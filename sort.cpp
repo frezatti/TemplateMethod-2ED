9a16aec3574cb1f39ee4055cd788b2e1d8f33b8a
Adriel Frezatti <Adrielfrezatti@gmail.com>
2023-10-03 09:25:31 -0300

    made a mistake in the sort function

--- sort.cpp
+++ sort.cpp
@@ -1,40 +1,37 @@
 #include "sort.h"
 
-Sort::Sort(Professor array[], int tamanho):
-    array(array),
-    arraySSort(array),
-    size(tamanho)
-{
-}
+Sort::Sort(Professor array[], int tamanho)
+    : array(array), arraySSort(array), size(tamanho) {}
 
-QString Sort::print(){
-    QString exit;
-      if(!array)throw QString("empty array");
-      for(int i = 0 ;i<size;i++){
-          exit+= QString::number(array[i].getId())+";";
-      }
-      return exit;
+QString Sort::print() {
+  QString exit;
+  if (!array)
+    throw QString("empty array");
+  for (int i = 0; i < size; i++) {
+    exit += QString::number(array[i].getId()) + ";";
+  }
+  return exit;
 }
-QString Sort::printSelectionSort(){
-    QString exit;
-      if(!array)throw QString("empty array");
-      for(int i = 0 ;i<size;i++){
-          exit+= QString::number(array[i].getId())+";";
-      }
-      return exit;
+QString Sort::printSelectionSort() {
+  QString exit;
+  if (!array)
+    throw QString("empty array");
+  for (int i = 0; i < size; i++) {
+    exit += QString::number(array[i].getId()) + ";";
+  }
+  return exit;
 }
-void Sort::selectionSort(){
-    if(!array)throw QString("empty array");
-    Professor minor;
-    for(int i =0;i<size;i++){
-        for(int j = 0;j<size-1;j++){
-            if(compare(array[i],array[j])){
-                minor = arraySSort[j];
-                arraySSort[j]=arraySSort[i];
-                arraySSort[i]=minor;
-            }
-        }
+void Sort::selectionSort() {
+  if (!array)
+    throw QString("empty array");
+  Professor minor;
+  for (int i = 0; i < size; i++) {
+    for (int j = i + 1; j < size - 1; j++) {
+      if (compare(array[i], array[j])) {
+        minor = arraySSort[j];
+        arraySSort[j] = arraySSort[i];
+        arraySSort[i] = minor;
+      }
     }
+  }
 }
-
-
